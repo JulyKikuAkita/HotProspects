3d3afca1a10ad4d380253722f9b96331ebd5b845
JulyKikuAkita <b92701105@gmail.com>
2022-02-24 21:54:36 -0800

    Reading custom values from the environment with @EnvironmentObject

--- HotProspects/ContentView.swift
+++ HotProspects/ContentView.swift
@@ -7,10 +7,34 @@
 
 import SwiftUI
 
-struct ContentView: View {
+@MainActor class User: ObservableObject {
+    @Published var name = "Nanachi"
+}
+
+// app crash if cannot find the EnvironmentObject
+struct EditView: View {
+    @EnvironmentObject var user: User
+
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        TextField("Name", text: $user.name)
+    }
+}
+
+struct DisplayView: View {
+    @EnvironmentObject var user: User
+
+    var body: some View {
+        Text(user.name)
+    }
+}
+
+struct ContentView: View {
+    @StateObject private var user = User()
+    var body: some View {
+        VStack {
+            EditView().environmentObject(user)
+            DisplayView().environmentObject(user)
+        }
     }
 }
 
