804034c2c90cf1bf829e31f710f3c83b59db4bc1
JulyKikuAkita <b92701105@gmail.com>
2022-02-26 22:08:00 -0800

    Understanding Swift’s Result type

--- HotProspects/ContentView.swift
+++ HotProspects/ContentView.swift
@@ -49,9 +49,20 @@ struct ContentView: View {
     @StateObject private var user = User()
     @State private var selectedTab = "Env"
     @ObservedObject var updater = DelayedUpdater()
+    @State private var output = ""
 
     var body: some View {
         TabView(selection: $selectedTab) {
+
+            Text(output)
+                .task {
+                    await fetchReadings()
+                }
+                .tabItem {
+                    Label("API", systemImage: "icloud.and.arrow.down")
+                }
+                .tag("Download")
+
             VStack {
                 EditView()
                 DisplayView()
@@ -71,6 +82,50 @@ struct ContentView: View {
                     Label("Fire", systemImage: "flame")
                 }
                 .tag("Flame")
+        }
+    }
+
+    /**
+     fetchReadingsNotFlexible() doesn’t give us a lot of flexibility –
+     what if we want to stash the work away somewhere and do something else while it’s running?
+     What if we want to read its result at some point in the future, perhaps handling any errors somewhere else entirely?
+     what if just want to cancel the work because it’s no longer needed?
+     -> use Result<String, Error>
+     */
+    func fetchReadingsNotFlexible() async {
+        do {
+            let url = URL(string: "https://hws.dev/readings.json")!
+            let (data, _) = try await URLSession.shared.data(from: url)
+            let readings = try JSONDecoder().decode([Double].self, from: data)
+            output = "Found \(readings.count) readings"
+        } catch {
+            print("Download error")
+        }
+    }
+
+    // Assign API result to fetchTask for flexibility: pass the object, cancel it, etc
+    func fetchReadings() async {
+        let fetchTask = Task { () -> String in
+            let url = URL(string: "https://hws.dev/readings.json")!
+            let (data, _) = try await URLSession.shared.data(from: url)
+            let readings = try JSONDecoder().decode([Double].self, from: data)
+            return "Found \(readings.count) readings"
+        }
+
+        let result = await fetchTask.result
+        //optino 1
+//        do {
+//            output = try result.get()
+//        } catch {
+//            output =  "Error: \(error.localizedDescription)"
+//        }
+
+        //option 2
+        switch result {
+        case .success(let str):
+            output = str
+        case .failure(let error):
+            output = "Error: \(error.localizedDescription)"
         }
     }
 }
