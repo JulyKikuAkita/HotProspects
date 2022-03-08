f396e7d023b3c78a82a8c996aa12d442cffa7b3d
JulyKikuAkita <b92701105@gmail.com>
2022-03-07 21:53:45 -0800

    Day 81: Adding Swift package dependencies in Xcode using SPM

--- HotProspects/ContentView.swift
+++ HotProspects/ContentView.swift
@@ -6,11 +6,18 @@
 //
 
 import SwiftUI
+import SamplePackage
 
 struct ContentView: View {
+    let possibleNumbers = Array(1...60)
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        Text(results)
+    }
+
+    var results: String {
+        let selected = possibleNumbers.random(7).sorted()
+        let strings = selected.map(String.init)
+        return strings.joined(separator: ", ")
     }
 }
 
