51a8fb7090f330e3d8b021ffec2e60becf6769d8
JulyKikuAkita <b92701105@gmail.com>
2022-03-07 21:44:05 -0800

    Day 81: Scheduling local notifications

--- HotProspects/ContentView.swift
+++ HotProspects/ContentView.swift
@@ -5,12 +5,42 @@
 //  Created by Ifang Lee on 2/23/22.
 //
 
+/**
+ local notifications are ones we schedule locally, and remote notifications (commonly called push notifications) are sent from a server somewhere.
+*/
+
 import SwiftUI
+import UserNotifications
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack {
+            Button("Request Permission") {
+                UNUserNotificationCenter.current().requestAuthorization(options: [.alert, .badge, .sound]) { success, error in
+                    if success {
+                        print("All set!")
+                    } else if let error = error {
+                        print(error.localizedDescription)
+                    }
+                }
+            }
+
+            /**
+             The content is what should be shown, and can be a title, subtitle, sound, image, and so on.
+             The trigger determines when the notification should be shown, and can be a number of seconds from now, a date and time in the future, or a location.
+             The request combines the content and trigger, but also adds a unique identifier so you can edit or remove specific alerts later on. If you don’t want to edit or remove stuff, use UUID().uuidString to get a random identifier.
+             */
+            Button("Schedule Notification") {
+                let content = UNMutableNotificationContent()
+                content.title = "Feed the dogs"
+                content.subtitle = "They look hungry"
+                content.sound = UNNotificationSound.default
+
+                let trigger = UNTimeIntervalNotificationTrigger(timeInterval: 5, repeats: false)
+                let request = UNNotificationRequest(identifier: UUID().uuidString, content: content, trigger: trigger)
+                UNUserNotificationCenter.current().add(request)
+            }
+        }
     }
 }
 
