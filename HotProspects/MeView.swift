fb5a51fa4586a293a3b45366a5137d7a111d8c5c
JulyKikuAkita <b92701105@gmail.com>
2022-03-15 21:59:29 -0700

    Day 84: Modifying state during view update, this will cause undefined behavior.

--- HotProspects/MeView.swift
+++ HotProspects/MeView.swift
@@ -12,6 +12,8 @@ import CoreImage.CIFilterBuiltins
 struct MeView: View {
     @State private var name = "Anonymous"
     @State private var emailAddress = "tmp@mail.com"
+    @State private var qrCode = UIImage()
+
     let context = CIContext()
     let filter = CIFilter.qrCodeGenerator()
 
@@ -31,6 +33,14 @@ struct MeView: View {
                     .resizable()
                     .scaledToFit()
                     .frame(width: 200, height: 200)
+                    .contextMenu {
+                        Button {
+                            let imageSaver = ImageSaver()
+                            imageSaver.writeToPhotoAlbum(image: qrCode)
+                        } label: {
+                            Label("Save to Photos", systemImage: "square.and.arrow.down")
+                        }
+                    }
             }
             .navigationTitle("Your code")
         }
@@ -41,7 +51,8 @@ struct MeView: View {
 
         if let outputImage = filter.outputImage {
             if let cgimg = context.createCGImage(outputImage, from: outputImage.extent) {
-                return UIImage(cgImage: cgimg)
+                qrCode = UIImage(cgImage: cgimg) // Cause "Modifying state during view update, this will cause undefined behavior."
+                return qrCode
             }
         }
         return UIImage(systemName: "xmark.circle") ?? UIImage()
